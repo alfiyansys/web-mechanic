d6d0cf253eba280f24de8a399ae967f7a93f7d15
alfiyansys <alfiyan.syamsuddin@gmail.com>
2015-11-11 13:28:33 +0900

    port forwarding

--- firewall/firewall.sh
+++ firewall/firewall.sh
@@ -5,7 +5,16 @@
 # Mother 8080 ~ Child 80
 # Mother 2222 ~ Child 22
 
+# make sure its enabled
 echo "1" > /proc/sys/net/ipv4/ip_forward
+
+# flush all tables
 iptables -F
-iptables -F -nat
+iptables -F -t -nat
+
+# provide internet connection
 iptables -t nat -A POSTROUTING -o ppp0 -j MASQUERADE
+
+# gives SSH to child
+iptables -t nat -A PREROUTING -p tcp -i ppp0 --dport 2222 -j DNAT --to-destination 192.168.1.10:22
+iptables -A FORWARD -p tcp -d 192.168.1.10 --dport 22 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
