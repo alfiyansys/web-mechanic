0f50ca1270d81c7277affef647c84b6394fda916
alfiyansys <alfiyan.syamsuddin@gmail.com>
2015-11-20 00:39:59 +0900

    solved, python not needed

--- scripts/capture.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-cmd = ['sudo', '/home/pi/scripts/capture.sh']
-proc = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
--- scripts/capture.sh
+++ scripts/capture.sh
@@ -3,13 +3,16 @@
 # International Exchange Program, Anan National College of Technology, Anan, Tokushima, Japan
 # make sure root has passwordless SSH access to server
 
+# run raspistill from cron with this crontab :
+# */3 * * * * /usr/bin/raspistill -vf -hf -w 1024 -h 768 -rot 270 -o "/var/www/current.jpg" && /home/pi/scripts/capture.sh
+
 #DATE=$(date +"%Y-%m-%d_%H%M")
 DATE=$(date +"%Y%m%d-%H%M")
 DEVICE="Kitagawa"
 SAVE="/var/www/current.jpg"
-/usr/bin/raspistill -vf -hf -w 1024 -h 768 -rot 270 -o $SAVE
+#/usr/bin/raspistill -vf -hf -w 1024 -h 768 -rot 270 -o $SAVE
 #make sure target directory exists
 #FNAME=$DEVICE-$DATE
 FNAME=$DATE
-scp $SAVE root@reenergy.info:/var/www/images/$FNAME.jpg
-ssh root@reenergy.info cp /var/www/images/$FNAME.jpg /var/www/html/img/current.jpg
+scp $SAVE root@reenergy.info:/var/www/images/kg/$FNAME.jpg
+ssh root@reenergy.info cp /var/www/images/kg/$FNAME.jpg /var/www/html/img/current.jpg
