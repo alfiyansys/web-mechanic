5cf014131a4f44c2e7300ebc6ef609949d71987c
alfiyansys <alfiyan.syamsuddin@gmail.com>
2015-11-20 07:46:06 +0900

    Serial initialization!!!

--- scripts/servo.sh
+++ scripts/servo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Servo init script by Muhammad Alfiyan Syamsuddin, 
 # International Exchange Program, Anan National College of Technology
-minicom -b 9600 -o -D /dev/ttyAMA* &
+# minicom -b 9600 -o -D /dev/ttyAMA* &
+stty -F /dev/ttyACM* 9600 cs8 cread clocal # serial initialization
 sleep 10
